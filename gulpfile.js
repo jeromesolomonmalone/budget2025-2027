7a17cbaced1186bd1a806cf4c64485eb85badad0
Roman Pigunov <jerome.malone@yandex.ru>
2025-05-14 22:34:25 +0300

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,58 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat-css');
-const plumber = require('gulp-plumber');
-const del = require('del');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const concat = require("gulp-concat-css");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const browserSync = require("browser-sync").create();
 
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist',
+      baseDir: "./dist",
     },
   });
 }
 
 function html() {
   return gulp
-    .src('src/**/*.html')
+    .src("src/**/*.html")
     .pipe(plumber())
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
   return gulp
-    .src('src/blocks/**/*.css')
+    .src("src/blocks/**/*.css")
     .pipe(plumber())
-    .pipe(concat('bundle.css'))
-    .pipe(gulp.dest('dist/'))
+    .pipe(concat("bundle.css"))
+    .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
   return gulp
-    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-    .pipe(gulp.dest('dist/images'))
+    .src("src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}")
+    .pipe(gulp.dest("dist/images"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
-  return del('dist');
+  return del("dist");
 }
 
 function watchFiles() {
-  gulp.watch(['src/**/*.html'], html);
-  gulp.watch(['src/blocks/**/*.css'], css);
-  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(["src/**/*.html"], html);
+  gulp.watch(["src/blocks/**/*.css"], css);
+  gulp.watch(["src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}"], images);
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, images));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
-exports.clean = clean;
-exports.images = images;
-exports.css = css;
 exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
 
 exports.build = build;
 exports.watchapp = watchapp;
